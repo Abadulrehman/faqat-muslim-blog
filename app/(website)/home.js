38da59824e40d390ef68128151eeba5dff46d270
AbdulRehman Muhammad Younis <Abdulrehman.younis@leam.ae>
2023-07-26 07:45:50 +0400

    chore: test commit

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Container from "@/components/container";
 import PostList from "@/components/postlist";
 
-export default function Post({ posts }) {
+export default function Home({ posts }) {
   return (
     <>
       {posts && (
@@ -17,11 +17,11 @@ export default function Post({ posts }) {
               />
             ))}
           </div>
-          <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
+          {/* <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
             {posts.slice(2, 14).map(post => (
               <PostList key={post._id} post={post} aspect="square" />
             ))}
-          </div>
+          </div> */}
           <div className="mt-10 flex justify-center">
             <Link
               href="/archive"
